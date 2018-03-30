ad0f15f902deb5b785f80411afcb98625b54ab0d
sanjay pinnaka <sanjaypinnaka@gmail.com>
2018-03-30 13:35:22 +0530

    make it better

--- app/controllers/labs_controller.rb
+++ app/controllers/labs_controller.rb
@@ -1,13 +1,19 @@
 class LabsController < ApplicationController
 	def index
-
+      
 	end	
 
 	def new 
-
+     @lab = current_user.labs.build
+     @experiment = current_user.experiments.build(experiment_params)
+	 @experiment.lab_id = params[:lab_id]
 	end 
 
 	def create 
 	end	
 	
+	private 
+	def lab_params
+		params.require(:experiment).permit(:title)		
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :experiments
   root 'welcome#index'
-
+  get '/lab' => "labs#index"
   get '/search' => "experiments#search"
 end
