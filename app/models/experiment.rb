79d113c96df4d238182717754f0377c036810db1
sanjay pinnaka <sanjaypinnaka@gmail.com>
2018-03-31 16:58:19 +0530

    <your commit message>

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,8 +1,9 @@
 class Experiment < ApplicationRecord
 	belongs_to :user
     belongs_to :lab
+    belongs_to :sem
+    belongs_to :collage
   #scope :searched_experiments -> where("title LIKE ?", "%#{params[:title]}%")
-
 end
 #scope :my_scope, ->(query) { includes(:table2).where(["table2.property LIKE (?)", "#{query}%"]) }
 #{}"name LIKE ? OR postal_code LIKE ?", "%#{search}%", "%#{search}%"
