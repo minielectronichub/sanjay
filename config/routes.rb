101d9e7dde78d0fa1cabbcbcc37a0490828d010f
sanjay pinnaka <sanjaypinnaka@gmail.com>
2018-04-04 18:28:11 +0530

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :experiments
   root 'welcome#index'
-  match 'show' => 'experiments#show',:as => :video
+ 
   get '/search' => "experiments#search"
 end
