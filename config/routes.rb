fea70627c00ce09ed2d523f0c8353a168a03f8a4
sanjay pinnaka <sanjaypinnaka@gmail.com>
2018-04-08 20:02:03 +0530

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
- 
+  root 'welcome#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  member do 
-    put :publish 
-  end
+ 
   devise_for :users
   resources :experiments 
-  
-  root 'welcome#index'
   get '/search' => "experiments#search"
 end
