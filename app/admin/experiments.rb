326377345ecd8ca1616fe50480aad05f2830bc39
sanjay pinnaka <sanjaypinnaka@gmail.com>
2018-04-08 11:38:06 +0530

    make it better

---
+++ app/admin/experiments.rb
@@ -0,0 +1,15 @@
+ActiveAdmin.register Experiment do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+permit_params :title, :description, :components, :youtube_link, :lab_id, :sem_id, :collage_id, :branch_id
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
