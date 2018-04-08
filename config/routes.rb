2e45b59274d831a6340ce85185771692f2cb8ce0
sanjay pinnaka <sanjaypinnaka@gmail.com>
2018-04-08 13:26:12 +0530

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
  
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-
+  member do 
+    put :publish 
+  end
   devise_for :users
   resources :experiments 
   
