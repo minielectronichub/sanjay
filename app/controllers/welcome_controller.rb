0a2afc7912649c0447f76c02a19b083b77654cfa
sanjay pinnaka <sanjaypinnaka@gmail.com>
2018-04-08 22:18:52 +0530

    make it better

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
     @experiments = Experiment.all    
     if(params[:query] && params[:query] != '')
       @search_query = params[:query]
-      @experiments = Experiment.where("title LIKE ?" , "%#{@search_query}%")
+      @experiments = Experiment.where("title LIKE ?" , "%#{@search_query.downcase}%")
       # @posts = Post.where(title: @search_query)
       respond_to do |format|
         @response = {status: "SUCCESS", experiments: @experiments}
