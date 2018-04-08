e966ba7d159716fb3e523c920e3b29b4412930b3
sanjay pinnaka <sanjaypinnaka@gmail.com>
2018-04-08 07:36:52 +0530

    make it better.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,14 @@
 class WelcomeController < ApplicationController
 
   def index
-
+      @experiments = Experiment.all    
+      if(params[:query] && params[:query] != '')
+      @search_query = params[:query]
+      @experiments = Experiment.where("title LIKE ?" , "%#{@search_query}%")
+      # @posts = Post.where(title: @search_query)
+      respond_to do |format|
+        @response = {status: "SUCCESS", experiments: @posts}
+        format.json {render json: @response}
   end
 
 end
