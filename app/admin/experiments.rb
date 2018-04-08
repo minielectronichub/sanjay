45315a7581da6f44da48d67a7a42471d0ca49bb0
sanjay pinnaka <sanjaypinnaka@gmail.com>
2018-04-08 13:06:33 +0530

    make it better

--- app/admin/experiments.rb
+++ app/admin/experiments.rb
@@ -4,6 +4,40 @@ ActiveAdmin.register Experiment do
 #
 # permit_params :list, :of, :attributes, :on, :model
 permit_params :title, :description, :components, :youtube_link, :lab_id, :sem_id, :collage_id, :branch_id
+
+
+member_action :publish, method: :put do 
+action_item :publish, only: :show do 
+    link_to "Publish", publish_admin_experiment_path(experiment), method: :put if !experiment.published_at? 
+end
+action_item :publish, only: :show do 
+    link_to "UnPublish", unpublish_admin_experiment_path(experiment), method: :put if !experiment.published_at? 
+end
+member_action :publish, method: :put do 
+    experiment = Experiment.find(params[:id])
+    experiment.update(published_at: Time.zone.now)
+    redirect_to admin_experiment_path(experiment)
+end 
+member_action :unpublish, method: :put do 
+    experiment = Experiment.find(params[:id])
+    experiment.update(published_at: nil)
+    redirect_to admin_experiment_path(experiment)
+end 
+
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
 # or
 #
 # permit_params do
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-
+ 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  member do 
+    put :publish 
+  end 
   devise_for :users
   resources :experiments
   root 'welcome#index'
