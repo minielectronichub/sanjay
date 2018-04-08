cee77185f73d2ecc0e9969535297385731f7a34c
sanjay pinnaka <sanjaypinnaka@gmail.com>
2018-04-08 07:46:16 +0530

    make it better

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,8 @@ class WelcomeController < ApplicationController
 
   def index
       @experiments = Experiment.all   
-
+       @experiments = Experiment.where("title LIKE ?", "%#{params[:title]}%")
+     render "index"
    
  end
 
