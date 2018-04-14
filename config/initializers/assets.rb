494ecb4a18c76ffa5a47ab57e7ba1902356f79e6
sanjay pinnaka <sanjaypinnaka@gmail.com>
2018-04-14 23:40:53 +0530

    make it better

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,8 +6,8 @@ Rails.application.config.assets.version = '1.0'
 
 
 Rails.application.config.assets.precompile += %w( style.css )
-Rails.application.config.assets.precompile += %w( app.js )
-Rails.application.config.assets.precompile += %w( jquery-1.11.1.min.js )
+#Rails.application.config.assets.precompile += %w( app.js )
+#Rails.application.config.assets.precompile += %w( jquery-1.11.1.min.js )
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
