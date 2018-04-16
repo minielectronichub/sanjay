cf0a7bff610706d33727d2775a86ee76ab102326
sanjay pinnaka <sanjaypinnaka@gmail.com>
2018-04-16 08:53:41 +0530

    better

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 #Rails.application.config.assets.precompile += %w( style.scss  *.js *.scss)
 
-
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( style.css )
 #Rails.application.config.assets.precompile += %w( app.js )
 #Rails.application.config.assets.precompile += %w( jquery-1.11.1.min.js )
