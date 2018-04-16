24a3df6dca7b6f4bc962e73ec39d731699a459e3
sanjay pinnaka <sanjaypinnaka@gmail.com>
2018-04-16 08:57:26 +0530

    better

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,8 @@ Rails.application.config.assets.version = '1.0'
 #Rails.application.config.assets.precompile += %w( style.scss  *.js *.scss)
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+# font-awesome fonts
+Rails.application.config.assets.precompile << %r{font-awesome/fonts/[\w-]+\.(?:eot|svg|ttf|woff2?)$}
 Rails.application.config.assets.precompile += %w( style.css )
 #Rails.application.config.assets.precompile += %w( app.js )
 #Rails.application.config.assets.precompile += %w( jquery-1.11.1.min.js )
