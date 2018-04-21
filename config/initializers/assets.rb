1dbde09f66ad0925f7c04ac133f96d3c9393b2bf
sanjay pinnaka <sanjaypinnaka@gmail.com>
2018-04-21 21:46:30 +0530

    make logo better

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,12 +2,12 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-#Rails.application.config.assets.precompile += %w( style.scss  *.js *.scss)
+Rails.application.config.assets.precompile += %w( application.scss)
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # font-awesome </fonts>
 Rails.application.config.assets.precompile << %r{font-awesome/fonts/[\w-]+\.(?:eot|svg|ttf|woff2?)$}
-Rails.application.config.assets.precompile += %w( style.css )
+#Rails.application.config.assets.precompile += %w( style.css )
 #Rails.application.config.assets.precompile += %w( app.js )
 #Rails.application.config.assets.precompile += %w( jquery-1.11.1.min.js )
 # Add additional assets to the asset load path.
