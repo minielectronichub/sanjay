b9258ec5f81d56217a884cac72b7bc9d3dd00cc7
sanjay pinnaka <sanjaypinnaka@gmail.com>
2018-04-21 21:48:09 +0530

    make logo better

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( application.scss)
+Rails.application.config.assets.precompile += %w( application.css)
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # font-awesome </fonts>
